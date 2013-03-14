dc23a329e6682df13713286f35ad6ac636da3f06
emilssolmanis <emils.solmanis@gmail.com>
2013-03-14 21:33:24 +0200

    fixed lab2 impl

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -8,24 +8,24 @@ def read_table(filename):
 
 def run(samples, weights, g):
     biased_samples = hstack((samples, array([[1] for _ in samples])))
-    return 1 / (1 + exp(-dot(biased_samples, weights.transpose()) / g))
+    return 1 / (1 + exp(-dot(biased_samples, weights) / g))
 
 
 def train_slp(samples, answers, max_epochs, g, train_coeff, err_limit):
     num_n = len(answers[0])
     num_w = len(samples[0])
 
-    weights = random.random((num_n, num_w + 1)) * 0.6 - 0.3
+    weights = random.random((num_w + 1, num_n)) * 0.6 - 0.3
     biased_samples = hstack((samples, array([[1] for _ in samples])))
     
     err = inf
     epoch = 0
     while epoch < max_epochs and err > err_limit:
-        y = 1 / (1 + exp(-dot(biased_samples, weights.transpose()) / g))
+        y = 1 / (1 + exp(-dot(biased_samples, weights) / g))
         error = answers - y
         grad = error / g * y * (1 - y)
 
-        wc = dot(transpose(grad), biased_samples) * train_coeff
+        wc = dot(biased_samples.transpose(), grad) * train_coeff
         weights += wc
 
         err = sum(error**2) / len(samples)
--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     for root, dirs, files in os.walk('.'):
         if root == '.':
             break
+    try:
+        dirs.remove('.git')
+    except ValueError:
+        pass
     for package in dirs:
         pack = __import__(package + ".test")
         pack.test.test()
