692845ccb5e92cf9e2b2e4bc6e676e584dd18686
emilssolmanis <emils.solmanis@gmail.com>
2013-04-07 00:30:13 +0300

    Kohonen SOM implemented

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -22,7 +22,7 @@ def kmeans(samples, num_clusters, max_epochs):
             closest[s_idx] = cluster
 
         cluster_centers = zeros((num_clusters, num_cols))
-        for i in range(len(cluster_centers)):
+        for i in range(num_clusters):
             cluster_samples = samples[where(closest == i)]
             if len(cluster_samples):
                 cluster_centers[i] = apply_along_axis(sum, 0, cluster_samples) / len(cluster_samples)
@@ -31,7 +31,28 @@ def kmeans(samples, num_clusters, max_epochs):
     
     return (cluster_centers, epoch)
 
-def kohonen(samples, s_low, s_high, learn_low, learn_high, delta, num_clusters, max_epochs):
+
+def kohonen(samples, s_start, s_end, learn_start, learn_end, delta, num_clusters, distances, max_epochs):
     mins = apply_along_axis(min, 0, samples)
     maxes = apply_along_axis(max, 0, samples)
-    pass
+    num_rows, num_cols = samples.shape
+
+    cluster_centers = random.random((num_clusters, num_cols)) * (maxes - mins) + mins
+
+    norm_samples = (samples - mins) / (maxes - mins)
+    s = s_start
+    learn = learn_start
+    
+    for epoch in range(max_epochs):
+        for s_idx, sample in enumerate(norm_samples):
+            closest = apply_along_axis(linalg.norm, 1, cluster_centers - sample).argmin()
+            for cluster in range(num_clusters):
+                d = distances[cluster, closest]
+                nfactor = exp(-((d**2) / (2 * s**2)))
+                cluster_centers[cluster] += learn * (sample - cluster_centers[cluster]) * nfactor
+        s += (s_end - s) * delta
+        learn += (learn_end - learn) * delta
+
+    cluster_centers = cluster_centers * (maxes - mins) + mins
+
+    return cluster_centers
