6bdcc272ffabc9c38890ca7693569ce3f05e12f0
emilssolmanis <emils.solmanis@gmail.com>
2013-04-07 00:35:17 +0300

    Lab 4: clusterization -- test cases done

--- lab4/test.py
+++ lab4/test.py
@@ -1,2 +1,24 @@
+import os
+from lab4 import read_table, kmeans, kohonen
+from numpy import array
+
 def test():
-    print "LAB4 unimplemented!"
+    print "--------------------------------------------------------------------------------"
+    print "----------------------------------LAB4------------------------------------------"
+    print "--------------------------------------------------------------------------------"
+
+    lab_root = os.path.dirname(__file__)
+    samples = read_table(lab_root + "/examples.txt");
+
+    centers, epoch = kmeans(samples, 4, 1000)
+    print "K-means finished in %d epochs"
+    print "Found cluster centers: "
+    for cluster in centers:
+        print "\t%s" % cluster
+    
+    dists = array([[0, 1, 1, 2], [1, 0, 2, 1], [1, 2, 0, 1], [2, 1, 1, 0]])
+    centers = kohonen(samples, 1, 0.2, 0.1, 0.01, 0.01, 4, dists, 100)
+
+    print "Found Kohonen SOM cluster centers: "
+    for cluster in centers:
+        print "\t%s" % cluster
