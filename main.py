e37f16137d1aba6aad18a20cb4d8cb5e4a49056a
emilssolmanis <emils.solmanis@gmail.com>
2013-04-07 04:23:16 +0300

    implemented possibility to run separate labs

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import sys
 import os
 
 def main():
-    for root, dirs, files in os.walk('.'):
-        if root == '.':
-            break
-    try:
-        dirs.remove('.git')
-    except ValueError:
-        pass
+    if len(sys.argv) == 1:
+        for root, dirs, files in os.walk('.'):
+            if root == '.':
+                break
+        try:
+            dirs.remove('.git')
+        except ValueError:
+            pass
+    else:
+        dirs = sys.argv[1:]
     for package in sorted(dirs):
         pack = __import__(package + ".test")
         pack.test.test()
